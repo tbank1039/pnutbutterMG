1077dd9d9710b6a808d075c200b288a063b260cf
Tyler Bank <tbank1039@gmail.com>
2017-07-01 22:25:10 +0000

    changing welcome page. adding pnut butter image

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -79,16 +79,21 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Peanut and Butter...
+                    Coming Soon.
                 </div>
 
-                <div class="links">
+                <div>
+                    <img src="/images/Peanut&Butter.jpg">
+                </div>
+
+                <!-- <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </body>
